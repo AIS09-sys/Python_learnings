fa585bca504974347cb97c3426a436608d10fc34
AIS09-sys <mynameisalish1@gmail.com>
2026-01-03 09:03:14 +0545

    Add files via upload

---
+++ 08learning_break&continue.py
@@ -0,0 +1,12 @@
+print("for break")
+for i in range(10):
+  if i == 5:
+   break        #i is for break end the loop
+  print(i)
+print("for continue")
+for j in range(10):
+  if j == 5:
+    continue       #j is for continue continue skip 
+  print(j)
+ 
+

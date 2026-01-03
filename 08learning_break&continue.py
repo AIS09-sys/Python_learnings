b4e76eea5e84d5e1f7686d66a507c226412ceb64
AIS09-sys <mynameisalish1@gmail.com>
2026-01-03 09:04:33 +0545

    Fix indentation and comments in break and continue examples

--- 08learning_break&continue.py
+++ 08learning_break&continue.py
@@ -6,7 +6,8 @@ for i in range(10):
 print("for continue")
 for j in range(10):
   if j == 5:
-    continue       #j is for continue continue skip 
+    continue       #j is for continue it is used for skip 
   print(j)
  
+
 
